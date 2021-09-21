bb5e5d80f54227e842f63569c90b73b85b9e4be4
lwardlegit <webcraftapps@gmail.com>
2021-09-20 22:35:05 -0400

    second

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="app">
+
+    <div id="topGrid">
+      <div>
+    <img id="logo" alt="Vue logo" src="./assets/emberspring.svg">
+      </div>
+
+      <div>
+      <section id="topbar" :class="section.id === toggled && 'toggleSectionActive'" v-for="section in sections" :key="section.id" @click="toggleStep(section.id)">
+                <h2>{{section.name}}</h2>
+      </section>
+    </div>
+  </div>
+
+    <div id="mainbodyGrid">
+      <div>
+        <p>hi</p>
+      </div>
+
+      <div>
+        <div v-if="toggled == 0"><Home/></div>
+        <div v-if="toggled == 1"><Services/></div>
+        <div v-if="toggled == 2"><About/></div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+import Services from './components/Services.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    Services,
+    About
+  },
+  data:function(){
+    return{
+      toggled: 0,
+      sections: [
+        {id: 0, name: 'Home', desc: 'home'},
+        {id: 1, name: 'Services', desc: 'services'},
+        {id: 2, name: 'About', desc: 'about'}
+      ],
+    }
+  },
+  methods:{
+    toggleStep(id){
+      this.toggled = id  
+    },
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Grotesk&display=swap');
+#app {
+  font-family: 'Space Grotesk', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 100%;
+  display: block;
+}
+
+#app div{
+  justify-content:center;
+}
+
+#logo{
+  width: 5em;
+  height: 5em;
+  border-radius: 100%;
+}
+#topGrid{
+  display: grid;
+  grid-template-columns: 15% 30%;
+}
+#topbar h2{
+  margin: 1em 3em 1em 3em;
+}
+#topbar h2:hover{
+  border-bottom: 1px solid orange;
+}
+.activetab{
+  color: blue;
+}
+#topGrid div{
+  display: flex;
+}
+#mainbodyGrid{
+  display: grid;
+  grid-template-columns: 15% 30%;
+  
+}
+#mainbodyGrid div{
+  margin: 1em;
+ 
+}
+</style>
---
+++ src/components/Services.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+
+    <div>
+      <h3>Services</h3>
+
+      <div id="contentBody">
+      <p>Emberspring offers custom development platforms and scalable services
+         to suit the needs of your custom applications</p>
+        
+      <h5>Web Apps</h5>
+      <p>Fully responsive front end applications utilizing the latest in web technology</p>
+
+      <h5>Mobile Applications</h5>
+      <p>Put your starup in your pocket with IOS and Android supported apps</p>
+      <img src="../assets/iphone.svg" height="50px" width="50px"/>
+
+      <h5>Api Development</h5>
+      <p>Original Ideas need data, we design pull, and analyze datasets to perform calculations that help you make business decisions faster than the competition</p>
+      </div>
+    </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Services',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#contentBody{
+  margin-top: 2em;
+}
+</style>
